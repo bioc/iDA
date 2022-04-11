06538c7752360c8eed7446185a9e1bc9b2c8a9b6
reesyxan <46451408+reesyxan@users.noreply.github.com>
2022-04-11 12:53:26 -0400

    add tests

--- tests/testthat/test.R
+++ tests/testthat/test.R
@@ -31,18 +31,26 @@ test_that("calculating within class scatter matrix works correctly", {
   expect_equal(ncol(wcs_mat), ncol(iris_input))
  })
 
-
-
 # test BCS
-
-
+test_that("calculating between class scatter matrix works correctly", {
+  iris_input <- select(iris, -c("Species"))
+  sp_dfs <- split(iris_input, f = iris$Species)
+  bcs_mat <- BCS(sp_dfs)
+  expect_equal(nrow(bcs_mat), ncol(iris_input))
+  expect_equal(ncol(bcs_mat), ncol(iris_input))
+})
 
 # test decomposesvd
-
-
-# test decomposeirlba
-
-
+test_that("decomposeSVD works correctly", {
+    iris_input <- select(iris, -c("Species"))
+    sp_dfs <- split(iris_input, f = iris$Species)
+    bcs_mat <- BCS(sp_dfs)
+    wcs_mat <- WCS(sp_dfs)
+    svd <- decomposeSVD(wcs_mat, bcs_mat, nu = 4)
+    expect_equal(ncol(svd$eigenvecs), 4)
+    expect_equal(nrow(svd$eigenvecs), 4)
+    expect_equal(dim(svd$eigenvalues), 4)
+})
 
 # test getSNN
 
